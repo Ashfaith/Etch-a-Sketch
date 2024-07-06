2256fb77e7b306d662dadd033c240b87b07b2047
Ashfaith <105349273+Ashfaith@users.noreply.github.com>
2024-07-06 16:25:56 +1200

    Added code to create a grid and select grid size

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const gameSpace = document.querySelector('#game-space');
+let tileAmnt = prompt("How many tiles would you like?")
 
-for (let i = 0 ; i < 256 ; i++) {
-    const tile = document.createElement('div');
-    tile.classList.add("tile-size");
-    gameSpace.appendChild(tile);
+function makeGrid() {
+    for (let i = 0 ; i < tileAmnt ; i++) {
+        for (let j = 0 ; j < tileAmnt ; j++){
+            const tile = document.createElement('div');
+            const tileSize = 500 / tileAmnt;
+            tile.style.height = `${tileSize}px`;
+            tile.style.width = `${tileSize}px`;
+            tile.classList.add("tile");
+            gameSpace.appendChild(tile);
+        }
+    }
 }
+makeGrid()
+
+const tiles = document.querySelectorAll('#game-space div')
+
+function draw(){
+    tiles.forEach(function(div) {
+        div.addEventListener('mouseover', function() {
+            this.classList.add('colour')
+        })
+    });
+}
+draw()
