ea36b49e32b2a0b9ff9c96dafec92f22faa4307f
Ashfaith <105349273+Ashfaith@users.noreply.github.com>
2024-07-08 15:40:41 +1200

    Added a button to reset drawing area and adjust size from input

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const gameSpace = document.querySelector('#game-space');
-let tileAmnt = prompt("How many tiles would you like?")
+const gridBtn = document.querySelector('#new-grid');
+let tileAmnt = 50;
+let tile;
+
+
+gridBtn.addEventListener('click', () => {
+    tileAmnt = prompt("How many tiles would you like?");
+    if (tileAmnt === null){
+        return
+    } else {
+    clearSpace();
+    makeGrid();
+    draw();
+    }
+});
 
 function makeGrid() {
     for (let i = 0 ; i < tileAmnt ; i++) {
         for (let j = 0 ; j < tileAmnt ; j++){
-            const tile = document.createElement('div');
+            tile = document.createElement('div');
             const tileSize = 500 / tileAmnt;
             tile.style.height = `${tileSize}px`;
             tile.style.width = `${tileSize}px`;
@@ -13,15 +27,22 @@ function makeGrid() {
         }
     }
 }
-makeGrid()
+makeGrid();
 
-const tiles = document.querySelectorAll('#game-space div')
+
+function clearSpace() {
+    while (gameSpace.firstChild){
+        gameSpace.firstChild.remove();
+    }
+}
+
 
 function draw(){
+    const tiles = document.querySelectorAll('#game-space div')
     tiles.forEach(function(div) {
         div.addEventListener('mouseover', function() {
-            this.classList.add('colour')
-        })
+            this.classList.add('colour');
+        });
     });
 }
-draw()
+draw();
